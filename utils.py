c82c9aac9139f8bdf18135b117df68e04b4c3ff4
Henry <xerxes50@yahoo.com>
2025-10-28 02:04:16 -0700

    fourth commit

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import hashlib
 from collections import Counter
 
-def analyze_string(text):
-    text = text.strip()
+def analyze_string(value: str) -> dict:
+    clean_value = value.strip()
     return {
-        "length": len(text),
-        "is_palindrome": text.lower() == text[::-1].lower(),
-        "unique_characters": len(set(text)),
-        "word_count": len(text.split()),
-        "sha256_hash": hashlib.sha256(text.encode()).hexdigest(),
-        "character_frequency_map": dict(Counter(text))
+        "length": len(clean_value),
+        "is_palindrome": clean_value.lower() == clean_value[::-1].lower(),
+        "unique_characters": len(set(clean_value)),
+        "word_count": len(clean_value.split()),
+        "sha256_hash": hashlib.sha256(clean_value.encode()).hexdigest(),
+        "character_frequency_map": dict(Counter(clean_value)),
     }
